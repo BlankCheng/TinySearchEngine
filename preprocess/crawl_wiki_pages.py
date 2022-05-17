b9814991e329e6b5ac9a4e283e0c6447325b7ea5
BlankCheng <913501223@qq.com>
2022-05-17 21:48:55 +0800

    Initial commit

---
+++ preprocess/crawl_wiki_pages.py
@@ -0,0 +1,69 @@
+import multiprocessing
+import requests
+import random
+import os
+import argparse
+import json
+from typing import List, Dict
+
+
+def worker(pid: int, page_urls: Dict, page_dir: str):
+    for page_name, page_url in page_urls.items():
+        if os.path.exists(os.path.join(page_dir, f'{page_name}.html')):
+            continue
+
+        print(f'Pid#{pid}: crawling {page_name}: {page_url}')
+        if random.random() < 0.01:
+            print(f"Num of pages: {len(os.listdir(page_dir))}")
+
+        try:
+            page_html = requests.get(url=page_url).text
+            with open(os.path.join(page_dir, f'{page_name}.html'), 'w') as f:
+                f.write(page_html)
+        except Exception as e:
+            print(e)
+
+
+def main():
+    if args.category:
+        categories = [args.category]
+    else:
+        categories = os.listdir(args.data_dir)
+
+    for cate in categories:
+        cate_dir = os.path.join(args.data_dir, cate)
+        page_dir = os.path.join(args.data_dir, cate, 'pages/')
+        page_url_path = os.path.join(cate_dir, 'page_url.json')
+        os.makedirs(page_dir, exist_ok=True)
+
+        with open(page_url_path, 'r') as f:
+            page_urls = json.load(f)
+
+        # split urls to processes
+        page_url_splits = [dict() for _ in range(args.n_processes)]
+        group = 0
+        for page, url in page_urls.items():
+            page_url_splits[group][page] = url
+            group = (group + 1) % args.n_processes
+
+        # multiprocess crawl
+        pool = multiprocessing.Pool(processes=args.n_processes)
+        for pid in range(args.n_processes):
+            pool.apply_async(worker, args=(pid, page_url_splits[pid], page_dir))
+
+        pool.close()
+        pool.join()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # path
+    parser.add_argument('--data_dir', type=str, default='../data/categories/')
+    # options
+    parser.add_argument('--category', type=str, default=None)
+    parser.add_argument('--n_processes', type=int, default=40)
+
+    args = parser.parse_args()
+
+    main()
+
---
+++ preprocess/get_wikipedia_category_tree.py
@@ -0,0 +1,40 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+MAX_LEVEL = 5
+# root = "Areas_of_computer_science"
+root = "Main_topic_classifications"
+
+def get_subtree(rets, subUrl, level):
+    if level>MAX_LEVEL:
+        return 0
+
+    try:
+        html = urlopen("https://en.wikipedia.org/wiki/Category:"+subUrl)
+    except BaseException:
+        print('fail')
+        exit()
+        return 0
+
+    bsObj = BeautifulSoup(html, 'html.parser')
+
+    bodys = bsObj.findAll("div", {"class":"CategoryTreeItem"})
+    for body in bodys:
+        kw = body.find("a")
+        contents = kw.text
+        kw = str(contents)
+        print(level,kw)
+        rets.append((level,kw))
+        get_subtree(rets, kw.replace(' ','_'), level+1)
+
+
+rets = []
+get_subtree(rets,root,1)
+
+with open(f'Wiki_tree-{root}-{MAX_LEVEL}.txt','w') as f:
+    for level,kw in rets:
+        try:
+            print(str(level)+'#'+kw,file=f)
+        except BaseException:
+            continue
