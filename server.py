166365c1f1b974013b68e0448f788aededd183f0
LMJJJJJJ <LMJJJJJJ@users.noreply.github.com>
2022-05-29 22:07:48 +0800

    update demo

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
+import linecache
 import os
 import os.path as osp
+import time
+
 import numpy as np
+from typing import Tuple, Union
 from tree.utils import CategoryInfo
 
 from flask import Flask, render_template, redirect, url_for, request, jsonify, make_response
@@ -10,6 +14,7 @@ import json
 data_folder = "./data"
 index_folder = osp.join(data_folder, "index")
 tree_folder = osp.join(data_folder, "tree")
+NUM_RESULT_PER_PAGE = 20
 
 app = Flask(__name__, static_folder="./demo/static", template_folder="./demo/template")
 
@@ -24,25 +29,57 @@ def search():
     if request.args:
         query = str(request.args.get("q"))
         method = str(request.args.get("m"))
+        page = str(request.args.get("p"))
+        if page == "None":
+            page = 1
+        if page <= 0:
+            page = 1
+        t0 = time.time()
+
+        q_correction, page_id_list = get_result(query, method)
+        n_result = len(page_id_list)
+
+        page_id_list = page_id_list[NUM_RESULT_PER_PAGE * (page - 1) : NUM_RESULT_PER_PAGE * page]
+
+        results = specify_results(page_id_list)
+
+        t_search = f"{time.time() - t0:.6}"
+
         return render_template("search.html",
                                query=query, method=method,
-                               results=get_dummy_results(query),
-                               n_result=100, t_search=0.01,
-                               q_correction="shit")
+                               results=results, n_result=n_result,
+                               t_search=t_search,
+                               q_correction=q_correction)
     else:
         return redirect(url_for("render_index"))
 
 
-def get_dummy_results(query):
-    category_info = CategoryInfo(data_folder)
+def get_result(query: str, method: str) -> Tuple[Union[None, str], Tuple]:
+    time.sleep(0.5)
+    return "shit", (5, 8, 9, 10, 11, 12, 13)
 
-    return [{
-        "page_id": 123456,
-        "page_url": "https://xxx.xxx.xxx/yy/zz",
-        "page_title": "This is a title",
-        "page_summary": f"This is {' and '.join(query.split())}. " * 50,
-        "page_main_categories": category_info.get_page_category_hierarchy(page_id=123)[0]
-    }] * 10
+
+def specify_results(page_id_list):
+    category_info = CategoryInfo(data_folder)
+    results = []
+    for page_id in page_id_list:
+        result = {}
+        result["page_id"] = page_id
+        result["page_title"] = linecache.getline(osp.join(index_folder, "pageid_title_map.txt"), page_id + 1)
+        if result["page_title"] is None:
+            continue
+        else:
+            result["page_title"] = result["page_title"].strip().split("-", 1)[1]
+        result["page_url"] = "https://en.wikipedia.org/wiki/" + result["page_title"].replace(" ", "_")
+        result["page_summary"] = f"Fuck you! " * 50   # TODO
+        result["page_main_categories"] = category_info.get_page_category_hierarchy(page_id=page_id)  # TODO
+        if len(result["page_main_categories"]) > 0:
+            result["page_main_categories"] = result["page_main_categories"][0]
+        results.append(result)
+    return results
+
+
+
 
 
 if __name__ == '__main__':
